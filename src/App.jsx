4d7e2196fda652096dad64fe692bf0fde0bad697
Nmn <Naman.tortalabz@hotmail.com>
2024-12-20 17:20:32 +0530

    final touches added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
 import Confetti from "react-confetti";
 import { cardImages } from "./data/data.json";
@@ -12,20 +12,19 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
-  const [confetti, setConfetti] = useState(false);
   const [disabled, setDisabled] = useState(false);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+
+  const showConfetti = useMemo(() => score === 6 && cards.every((card) => card.matched) , [score, cards]);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random(), matched: false }));
     setCards(shuffledCards);
-    setTurns(0);
-    // setConfetti(false);
-    setScore(0);
     setGameStarted(true);
+
   };
 
   // compare both selected cards
@@ -62,9 +61,15 @@ const App = () => {
     setDisabled(false);
   };
 
+  const onReset = () => {
+    window.location.reload();
+  };
+
   if (!gameStarted) {
     return (
       <div className="landing-screen">
+      {<Confetti  />}
+        <h1>{'Memory Game'}</h1>
         <motion.button
           className="start-button"
           whileHover={{ scale: 1.2 }}
@@ -77,17 +82,17 @@ const App = () => {
     );
   }
 
-  // console.log(cards);
 
   return (
     <div className="App">
-      <h1>Memory Game</h1>
+      {showConfetti && <Confetti />}
+      <h1 className={ showConfetti ? "won": ''} >{ !showConfetti  ? 'Memory Game': 'Congratulations'}</h1>
       <div className="score-container" >
         <div>
           <span>Score: {score}</span>
           <span>Turns: {turns}</span>
         </div>
-        <button onClick={() => setGameStarted(false)}>Reset</button>
+        <button onClick={onReset}>Reset</button>
       </div>
       <div className="card-grid">
         {cards.map((card) => (
